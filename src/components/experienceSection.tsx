d47e90606311e4a2a167f1073652fb780a680f1e
teamblack <juanarias121233@gmail.com>
2026-05-19 20:45:08 +0200

    menos un import que falto quitar

--- src/components/experienceSection.tsx
+++ src/components/experienceSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import {
